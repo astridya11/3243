00d576c07fdcaa66bd40a26dbe6723e9c4cb0017
astridya11 <90853398+astridya11@users.noreply.github.com>
2024-08-15 00:18:13 +0800

    config.php uploaded

---
+++ config.php
@@ -0,0 +1,7 @@
+<!--define constants for paths-->
+<?php
+define('BASE_PATH', dirname(__DIR__) . '/');
+define('CONTROLLER_PATH', BASE_PATH . 'controller/');
+define('MODEL_PATH', BASE_PATH . 'model/');
+define('VIEW_PATH', BASE_PATH . 'view/');
+?>
