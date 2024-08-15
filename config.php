4470585640a41f0cc5283ea7ce844a7f2df98d35
astridya11 <90853398+astridya11@users.noreply.github.com>
2024-08-16 01:35:27 +0800

    modified config file

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <!--define constants for paths-->
 <?php
-define('BASE_PATH', dirname(__DIR__) . '/');
+define('BASE_PATH', __DIR__ . '/');
 define('CONTROLLER_PATH', BASE_PATH . 'controller/');
 define('MODEL_PATH', BASE_PATH . 'model/');
 define('VIEW_PATH', BASE_PATH . 'view/');
